260a9806c65175265a47dfc4fe656199bb73ff74
Tom <thoward12@outlook.com>
2019-04-17 10:02:20 -0400

    starter code

---
+++ lab-11/bst.h
@@ -0,0 +1,24 @@
+#ifndef __BINTREE_H__
+#define __BINTREE_H__
+
+#include <ostream>
+
+#ifndef NULL
+#define NULL 0x00
+#endif
+
+#ifndef MAX
+#define MAX(a,b) ((a > b) ? a : b)
+#endif
+
+class BSTNode
+{
+
+};
+
+class BSTree
+{
+
+};
+
+#endif
